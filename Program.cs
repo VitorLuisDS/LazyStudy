9df74fbba31254b43d533e291ddf66a7ee90ee56
Vitor Silva <vitor.silva@craftablesoftware.com>
2022-02-24 15:19:26 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("LazyThreadSafetyMode - None");
+ParallelExperiment(LazyThreadSafetyMode.None);
+Task.Delay(500);
+
+Console.WriteLine("LazyThreadSafetyMode - PublicationOnly");
+ParallelExperiment(LazyThreadSafetyMode.PublicationOnly);
+Task.Delay(500);
+
+Console.WriteLine("LazyThreadSafetyMode - ExecutionAndPublication");
+ParallelExperiment(LazyThreadSafetyMode.ExecutionAndPublication);
+Task.Delay(500);
+
+Console.ReadLine();
+
+void ParallelExperiment(LazyThreadSafetyMode lazyThreadSafetyMode)
+{
+    Lazy<User> user = new Lazy<User>(lazyThreadSafetyMode);
+
+    Parallel.For(0, 7, new ParallelOptions { MaxDegreeOfParallelism = 3 }, (i) =>
+    {
+        Console.WriteLine($"#{i} Thread: {Thread.CurrentThread.ManagedThreadId:00} - HashCode: {user.Value.GetHashCode()}");
+    });
+
+    Console.WriteLine("---------------");
+    Console.WriteLine($"HashCode: {user.Value.GetHashCode()}");
+    Console.WriteLine("---------------\n\n");
+}
+
+public class User
+{
+    public string Name { get; set; }
+
+    public User()
+    {
+        Console.WriteLine("Creating user...");
+        Task.Delay(1000).Wait();
+        Console.WriteLine($"User created! - {this.GetHashCode()}");
+    }
+}
